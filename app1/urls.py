585f4542f1bce5e9c15adc5d40bdbec9365ebaf9
Nimish Jain <nimishjain.005@gmail.com>
2021-04-17 19:31:39 +0530

    first commit

---
+++ app1/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from app1 import views
+
+app_name = "app1"
+
+
+
+urlpatterns = [
+    path('',views.page1, name = "page1"),
+    path('page2/',views.page2,name = "page2"),
+]
